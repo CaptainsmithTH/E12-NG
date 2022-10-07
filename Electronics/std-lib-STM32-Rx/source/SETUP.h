78fb7c75d6be226381c8dfb6e79b9a9f7a1c9ea9
CaptainsmithTH <boonjohn91@gmail.com>
2022-10-07 18:00:43 +0700

    Correction1

--- Electronics/std-lib-STM32-Rx/source/SETUP.h
+++ Electronics/std-lib-STM32-Rx/source/SETUP.h
@@ -2,13 +2,4 @@
 #include "GPIO.h"
 #include "REMAP.h"
 
-void E12Setup()
-{
-	remap_SPI1();
-	general_Setup();
-	
-	RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
-	RCC->AHBENR |= RCC_AHBENR_DMA1EN;
-	
-	SPI1->CR1 |= SPI_CR1_CRCEN;
-}
+void E12Setup();
